bfdbd5c3d4a09f6152715f3685e58bfce8cf0834
Kacper Żarna <kacper-zarna@wp.pl>
2022-07-27 20:31:55 +0200

    Kod 27.07.2022 20:30

--- components/renderHomePage.js
+++ components/renderHomePage.js
@@ -1,11 +1,25 @@
+const clearContentContainer = (contentContainer) => {
+  contentContainer.innerHTML = "";
+};
+
+const createH2 = () => {
+  const h2 = document.createElement("h2");
+  h2.textContent = "Welcome!";
+  return h2;
+};
+
+const createParagraph = () => {
+  const p = document.createElement("p");
+  p.textContent =
+    "This is a simple web page written in vanilla JavaScript, used as a practice project in frontend courses at Software Development Academy. Block subject: Firebase.";
+  return p;
+};
+
 export default function () {
-    const contentContainer = document.querySelector(".content");
-    contentContainer.innerHTML = "";
-    const h2 = document.createElement("h2");
-    h2.textContent = "Welcome!";
-    const p = document.createElement("p");
-    p.textContent =
-      "This is a simple web page written in vanilla JavaScript, used as a practice project in frontend courses at Software Development Academy. Block subject: Firebase.";
-    contentContainer.appendChild(h2);
-    contentContainer.appendChild(p);
-  }
+  const contentContainer = document.querySelector(".content");
+  clearContentContainer(contentContainer);
+  const h2 = createH2();
+  const p = createParagraph();
+  contentContainer.appendChild(h2);
+  contentContainer.appendChild(p);
+}
